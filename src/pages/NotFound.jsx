418433e7dbc82eeae4fa49ba37047f16fcd205a6
david93dev <david.flor@maisunifacisa.com.br>
2025-09-22 19:53:38 -0300

    feat:  page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='w-full h-96 flex justify-center items-center text-center flex-col space-y-2'>
+      <h1 className='scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance'>404</h1>
+      <p className='scroll-m-20 text-center text-2xl font-semibold tracking-tight text-balance'>Not Found</p>
+    </div>
   )
 }
 
